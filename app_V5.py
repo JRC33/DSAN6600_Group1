2cbee897051d0434141045dd1cf8f8deb8fe0eeb
Matthew Moriarty <matthewmoriarty@Matthews-MacBook-Pro-4.local>
2023-11-29 14:57:15 -0500

    changing test tab to make image smaller

--- app_V5.py
+++ app_V5.py
@@ -181,8 +181,13 @@ def main():
                 true_letter = letter
 
                 ## note the changes i made to this line.  I need to add the st.pyplot and the .figure here at the end
-                st.pyplot(plt.imshow(df.reshape(28,28,1), cmap='gray').figure)
+                fig, ax = plt.subplots(1, 1, figsize = (4, 4))
+                plt.imshow(df.reshape(28,28,1), cmap='gray')
                 plt.axis('off')
+                st.pyplot(fig.figure)
+
+                # st.pyplot(plt.imshow(df.reshape(28,28,1), cmap='gray').figure)
+                # plt.axis('off')
                 plt.show()
 
                 #st.markdown(pred_letter)
