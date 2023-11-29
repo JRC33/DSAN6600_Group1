c6f7a81898d72a7f27db7492e8a6379e228bc02c
Matthew Moriarty <matthewmoriarty@Matthews-MacBook-Pro-4.local>
2023-11-29 14:46:43 -0500

    changing test image tab image

--- app_V5.py
+++ app_V5.py
@@ -179,7 +179,7 @@ def main():
             true_letter = letter
 
             ## note the changes i made to this line.  I need to add the st.pyplot and the .figure here at the end
-            st.pyplot(plt.imshow(test.iloc[image_num][1:].values.reshape(28,28,1), cmap='gray').figure)
+            st.pyplot(plt.imshow(df.reshape(28,28,1), cmap='gray').figure)
             plt.axis('off')
             plt.show()
 
